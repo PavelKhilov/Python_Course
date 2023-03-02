db2af55887365602c97043ec73a577269c5a0ed8
Pavel <khilov.pavel@eyandex.ru>
2023-03-02 20:11:12 +0300

    Teacher variant HW2

---
+++ Seminar2/HW.py
@@ -0,0 +1,5 @@
+ticket_num = input()
+sum_first = int(ticket_num[0] + ticket_num[1] + ticket_num[2])
+sum_last = int(ticket_num[3] + ticket_num[4] + ticket_num[5])
+
+print = (f"The ticket is lucky: {sum_first == sum_last}")
