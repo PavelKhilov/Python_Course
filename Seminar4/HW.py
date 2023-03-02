6e2e2649640075579a760811fb9b6439e5e042b6
Pavel <khilov.pavel@eyandex.ru>
2023-03-02 21:15:22 +0300

    Teacher variant HW3

--- Seminar4/HW.py
+++ Seminar4/HW.py
@@ -23,3 +23,12 @@ print(list_num)
 b = int(input())
 m = min(list_num, key=lambda x: abs(x - b))
 print(m)
+
+# реализация 1 задача 20
+ang_dict = {"AEIOULNSTRАВЕИНОРСТ": 1, "DGДКЛМПУ": 2, 
+         "BCMPБГЁЬЯ": 3, "FHVWYЙЫ": 4, "KЖЗХЦЧ": 5, 
+         "JXШЭЮ": 8, "QZФЩЪ": 10}
+
+word = input()
+
+print = sum([i[1]for i in ang_dict.items() for j in word if j.upper() in i[0]])
